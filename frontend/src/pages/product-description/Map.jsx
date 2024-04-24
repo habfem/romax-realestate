c61def5fd1299589e96f1dfc6e2159660f73791e
mac <habeebmustapha96@gmail.com>
2024-04-24 10:39:11 +0100

    update

--- frontend/src/pages/product-description/Map.jsx
+++ frontend/src/pages/product-description/Map.jsx
@@ -22,8 +22,8 @@ const Map = () => {
     fetchData();
   }, [id])
 
-  const streetViewURL = `https://www.google.com/maps/embed?pb=!3m2!1sen!2sng!4v1707140494131!5m2!1sen!2sng!6m8!1m7!1s5QPqTESWceMIwAMjkXcr_g!2m2!1d9.00629720178299!2d7.270159733240058!3f175.6531677246094!4f-1.6252365112304688!5f0.7820865974627469`;
-  const satelliteViewURL = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.628832061897!2d7.2669306747132225!3d9.006258891054172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e7195d1abacc9%3A0xc905ee88d56fa2ff!2sNnamdi%20Azikiwe%20International%20Airport!5e0!3m2!1sen!2sng!4v1707140705524!5m2!1sen!2sng`;
+  const streetViewURL = product.streetView;
+  const satelliteViewURL = product.sataliteView;
 
   const openInNewTab = (url) => {
     window.open(url, '_blank');
